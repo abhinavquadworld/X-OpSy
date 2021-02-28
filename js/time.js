ba94eae2b8542e06235020163b20cd96068fb8cd
abhinavquadworld <60970528+abhinavquadworld@users.noreply.github.com>
2021-02-28 16:15:17 +0530

    time ok

--- js/time.js
+++ js/time.js
@@ -1,21 +1,15 @@
-function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-  
-  function startTime() {
+function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
-    // add a zero in front of numbers<10
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-    t = setTimeout(function() {
-      startTime()
-    }, 500);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
   }
-  startTime();
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
