069f0ec47a9d39959a95b363eba6b0ea25b52bab
abhinavquadworld <60970528+abhinavquadworld@users.noreply.github.com>
2021-06-15 05:27:36 +0530

    nice

--- js/modalw.js
+++ js/modalw.js
@@ -1,8 +1,8 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModalW");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtnW");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
